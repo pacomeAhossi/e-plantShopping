7797c3c4aa13a2e2095055b74cc6d7fa710362a7
Pacome AHOSSI <pacomeahossi175@gmail.com>
2025-03-21 10:26:10 -0400

    AddtoCart btn disabled if plant added to cart

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -3,6 +3,7 @@ import './ProductList.css'
 import CartItem from './CartItem';
 import { useDispatch } from "react-redux";
 import { addItem } from "./CartSlice";
+
 function ProductList({ onHomeClick }) {
     const [showCart, setShowCart] = useState(false);
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
@@ -235,6 +236,7 @@ function ProductList({ onHomeClick }) {
         color: 'white',
         fontSize: '30px',
         textDecoration: 'none',
+        textAlign: 'center',
     }
 
     const handleHomeClick = (e) => {
@@ -281,7 +283,7 @@ function ProductList({ onHomeClick }) {
                 </div>
                 <div style={styleObjUl}>
                     <div> <a href="#" onClick={(e) => handlePlantsClick(e)} style={styleA}>Plants</a></div>
-                    <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg>  </h1>  {addedToCart && Object.keys(addedToCart).length > 0 ? Object.keys(addedToCart).length : ''} </a></div>
+                    <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path> </svg>  </h1> <i className='cart_quantity_count'>{addedToCart && Object.keys(addedToCart).length > 0 ? Object.keys(addedToCart).length : ''}</i>  </a></div>
                 </div>
             </div>
             {!showCart ? (
@@ -294,10 +296,15 @@ function ProductList({ onHomeClick }) {
                                 <div className="product-card" key={plantIndex}>
                                     <div className="product-title">{plant.name}</div>
                                     <img className="product-image" src={plant.image} alt={plant.name} />
-                                    <div className="product-title">{plant.cost}</div>
+                                    <div className="product-price">{plant.cost}</div>
                                     <div className="product-title">{plant.description}</div>
-                                    {/*Similarly like the above plant.name show other details like description and cost*/}
-                                    <button  className="product-button" onClick={() => handleAddToCart(plant)}>Add to Cart</button>
+                                    {/*si le produit se retrouve dans le sate addedToCart, alors on désactive le bouton ajouté*/}
+                                    <button  className={`product-button ${addedToCart[plant.name] ? 'added-to-cart' : ''}`}  
+                                        onClick={() => handleAddToCart(plant)}
+                                        disabled={addedToCart[plant.name]}
+                                    >
+                                        Add to Cart
+                                    </button>
                                 </div>
                                 ))}
                             </div>
